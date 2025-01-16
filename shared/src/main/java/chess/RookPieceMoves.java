bc1ac5307c0ddffdf83108fa2446747dee1932ba
Jansen Nye <jansen05@students.byu.edu>
2025-01-16 14:04:09 -0700

    idk what I added

--- shared/src/main/java/chess/RookPieceMoves.java
+++ shared/src/main/java/chess/RookPieceMoves.java
@@ -9,13 +9,13 @@ public class RookPieceMoves {
     private final Collection<ChessMove> RookMoves;
 
     public RookPieceMoves(ChessPosition myPosition, ChessBoard myBoard) {
-        int[][] queenDirections = {
+        int[][] rookDirections = {
                 {1, 0},   // Up
                 {0, 1},   // Right
                 {0, -1},  // Left
                 {-1, 0},  // Down
         };
-        this.RookMoves = findDistanceMoves(myBoard, myPosition, queenDirections);
+        this.RookMoves = findDistanceMoves(myBoard, myPosition, rookDirections);
     }
 
     public Collection<ChessMove> getRookMoves() {
