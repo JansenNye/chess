9988b1fd2306183434a91f070f643cc04addfe22
JansenNye <jansennye@Jansens-MacBook-Air-8.local>
2025-03-26 13:04:21 -0600

    Server.java and starting chessclient

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,4 +1,24 @@
 package ui;
-import com.google.gson.Gson;
+import serverfacade.ServerFacade;
+import exception.ResponseException;
+import model.AuthData;
+import model.GameData;
+import service.results.ListGamesResult;
+import service.results.ListGamesResult.GameInfo;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ChessClient {
+    private final ServerFacade server;
+    private String authToken = null;
+    private State state = State.LOGGEDOUT;
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+    }
+
+    
 }
+
+
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -6,6 +6,7 @@ import service.ClearService;
 import service.GameService;
 import service.UserService;
 import spark.Spark;
+
 import dataaccess.DatabaseManager;
 
 public class Server {
@@ -20,21 +21,21 @@ public class Server {
             return -1;
         }
 
-        // 2) Set up Spark
+        // Set up Spark
         Spark.port(desiredPort);
         Spark.staticFiles.location("web");
 
-        // 3) Instantiate DAOs (they now have a valid database to connect to)
+        // Instantiate DAOs
         var userDAO = new UserDAOMySQL();
         var gameDAO = new GameDAOMySQL();
         var authDAO = new AuthDAOMySQL();
 
-        // 4) Instantiate Services
+        // Instantiate Services
         var clearService = new ClearService(userDAO, gameDAO, authDAO);
         var userService = new UserService(userDAO, authDAO);
         var gameService = new GameService(gameDAO, authDAO);
 
-        // 5) Instantiate Handlers
+        // Instantiate Handlers
         var clearHandler = new ClearHandler(clearService);
         var registerHandler = new RegisterHandler(userService);
         var loginHandler = new LoginHandler(userService);
@@ -43,7 +44,7 @@ public class Server {
         var createGameHandler = new CreateGameHandler(gameService);
         var joinGameHandler = new JoinGameHandler(gameService);
 
-        // 6) Register Routes
+        // Register Routes
         Spark.delete("/db", clearHandler);
         Spark.post("/user", registerHandler);
         Spark.post("/session", loginHandler);
@@ -52,7 +53,7 @@ public class Server {
         Spark.post("/game", createGameHandler);
         Spark.put("/game", joinGameHandler);
 
-        // 7) Wait for Spark to finish initialization
+        // Wait for Spark to finish initialization
         Spark.awaitInitialization();
         return Spark.port();
     }
