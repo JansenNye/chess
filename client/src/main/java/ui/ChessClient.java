db60a77e752f29c195b58e64d87c77f768d16790
JansenNye <jansennye@Jansens-MacBook-Air-8.local>
2025-03-26 13:09:02 -0600

    structure & register/login/logout

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -7,6 +7,7 @@ import service.results.ListGamesResult;
 import service.results.ListGamesResult.GameInfo;
 
 import java.util.Arrays;
+
 import java.util.List;
 
 public class ChessClient {
@@ -16,6 +17,60 @@ public class ChessClient {
 
     public ChessClient(String serverUrl) {
         server = new ServerFacade(serverUrl);
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.trim().split("\\s+");
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            var cmd = tokens.length > 0 ? tokens[0].toLowerCase() : "help";
+
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "logout" -> logout();
+                case "create" -> createGame(params);
+                case "list" -> listGames();
+                case "play", "join" -> joinGame(params);
+                case "observe" -> observeGame(params);
+                case "help" -> help();
+                case "quit" -> "quit";
+                default -> "No clue what that command is. Type help broski";
+            };
+        }
+        catch (ResponseException e) {
+            return e.getMessage();
+        }
+    }
+
+    // Register
+    private String register(String... params) throws ResponseException {
+        if (params.length == 3) {
+            AuthData data = server.register(params[0], params[1], params[2]);
+            authToken = data.authToken();
+            state = State.LOGGEDIN;
+            return "Registered & logged in as: " + data.username();
+        } throw new ResponseException(400, " Expected: register <username> <password> <email>");
+    }
+
+    //Login
+    private String login(String... params) throws ResponseException {
+        if (params.length == 2) {
+            AuthData data = server.login(params[0], params[1]);
+            authToken = data.authToken();
+            state = State.LOGGEDIN;
+            return "Logged in as: " + data.username();
+        }
+        throw new ResponseException(400, "Expected: login <username> <password>");
+    }
+
+    //Logout
+    private String logout() throws ResponseException {
+        ensureLoggedIn();
+        server.logout(authToken);
+        authToken = null;
+        state = State.LOGGEDOUT;
+        return "Logged out.";
     }
 
     
